4e1186be77e10c596d17b43b2888176da04ba740
mebegu <mehmetgurcay@ku.edu.tr>
2017-03-12 12:51:53 +0300

    conn check

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ const config = require('./config.json');
 
 mongoose.Promise = global.Promise;
 const conn = mongoose.connect(config.db);
+mongoose.connection.once('connected', function() {
+	console.log("Connected to database")
+});
 
 app.use(bodyParser.json());
 app.use(cookieParser());
